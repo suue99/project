07e4b663c8f675c8a6d6067b7b8a6ec9b5e272e3
Susan Olofin <Susan Olofin>
2024-05-14 01:44:49 +0100

    new fonts

--- src/Components/Hompage/Home.js
+++ src/Components/Hompage/Home.js
@@ -4,6 +4,11 @@ import React, { useState, useEffect } from 'react';
 import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../../Firebase";
 import { useNavigate } from 'react-router-dom';
+import { FaUserDoctor } from "react-icons/fa6";
+import { LuDumbbell } from "react-icons/lu";
+import { FaHandsHelping } from "react-icons/fa";
+
+
 
 function Home() {
     const location = useLocation();
@@ -28,6 +33,8 @@ function Home() {
     }
 
     return (
+
+        <div>
         <div className="home-container">
             <div className="greeting">
                 {location.state && location.state.welcomeMessage && (
@@ -62,6 +69,45 @@ function Home() {
                     <Link to={'/Signup'}> <Button className="p-3">Get started</Button></Link>
                 </span>
             )}
+        </div>
+
+       <div className="empty">
+
+       </div>
+
+       
+
+
+        <div className="options-container">
+            <span className="expert">
+                <h1 className="h-1">Expert Guidance <FaUserDoctor className="icon" /></h1>
+               
+                <p className="para-one">Connect with experienced
+                    physiotherapists for tailored
+                    advice and treatment plans.
+                </p>
+            </span>
+
+            <span className="perfect">
+                <h1  className="h-1">Your Perfect Plan <LuDumbbell className="icon" /></h1>
+            
+                <p  className="para-one">Discover exercises tailored 
+                    specifically to your condition
+                    and goals
+                </p>
+            </span>
+
+            <span className="always">
+                <h1  className="h-1">Always here for you <FaHandsHelping  className="icon" /></h1>
+             
+                <p  className="para-one">Get the help you need
+                    whenever you need it. 
+                    From expert advice to 
+                    exercise modification
+                </p>
+            </span>
+        </div>
+
         </div>
     );
 }
